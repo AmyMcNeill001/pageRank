088daa13d009bb098172bc789a49a3291a650e18
Amy McNeill <amymcneill@Amys-MacBook-Pro.local>
2016-06-22 23:04:03 +0900

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Amy McNeill on 6/22/16.
+//  Copyright (c) 2016 Amy McNeill. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
